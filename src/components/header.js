54950545a34a99e963fec5853863f52d2bdecf7f
Andrea <andrea.marrao92@gmail.com>
2020-05-10 17:33:03 +0200

    added image menu, and fix styles

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,10 @@ import TopBar from "./topbar"
 import IconSocial from "./IconSocial"
 
 import Image1 from "../images/home.jpg"
-import Image2 from "../images/2.jpg"
-import Image3 from "../images/3.jpg"
-
+import ImageAbout from "../images/about.jpg"
+import ImageSkills from "../images/skill.jpg"
+import ImageProject from "../images/projects.jpg"
+import ImageContact from "../images/contact.jpg"
 
 const Header = () => {
     const elements = [
@@ -14,16 +15,31 @@ const Header = () => {
             title: "Home",
             image: Image1,
             path: "/",
+            class: "menu__item-img",
         },
         {
-            title: "About Me",
-            image: Image2,
-            path: "/about-me",
+            title: "About",
+            image: ImageAbout,
+            path: "/about",
+            class: "menu__item-img",
         },
         {
-            title: "Contact Me",
-            image: Image3,
-            path: "/contact-me",
+            title: "Skills",
+            image: ImageSkills,
+            path: "/skills",
+            class: "menu__item-img imageMenuTop",
+        },
+        {
+            title: "My work",
+            image: ImageProject,
+            path: "/my-work",
+            class: "menu__item-img imageMenuMiddle",
+        },
+        {
+            title: "Contact",
+            image: ImageContact,
+            path: "/contact",
+            class: "menu__item-img imageMenuBottom",
         },
     ]
 
@@ -33,7 +49,7 @@ const Header = () => {
         menuItems.push(
             <div className="menu__item">
                 <Link to={value.path} className="menu__item-link">{value.title}</Link>
-                <img className="menu__item-img" src={value.image} alt="Some"/>
+                <img className={value.class} src={value.image} alt="Some"/>
                 <div className="marquee">
                     <div className="marquee__inner" aria-hidden="true">
                         <span>{value.title}</span>
