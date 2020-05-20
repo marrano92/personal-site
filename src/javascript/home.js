fa979eb64ce3b88862ad0c403cc2b33c98e58dca
Andrea <andrea.marrao92@gmail.com>
2020-05-20 17:59:14 +0200

    add about in home

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -5,6 +5,8 @@ if (typeof window !== 'undefined' && document) {
         if (!event.target.matches('#nav-icon')) return;
         document.querySelector('#nav-icon').classList.toggle('open');
         document.querySelector('.menuDiv').classList.toggle('is-none');
+        document.querySelector('.appHeader').classList.add('p-fixed');
+
 
     }, false);
 }
